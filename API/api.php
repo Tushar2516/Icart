1301969822b66acd28790f8cf07e17633b1024ca
Tushar Patel <93286342+tushar4242@users.noreply.github.com>
2022-03-07 02:24:34 -0500

    Add files via upload

--- API/api.php
+++ API/api.php
@@ -7,8 +7,8 @@ header('Access-Control-Allow-Headers: Content-Type');
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
-//$dbname = "shopping_cart"; /* Database name */
 $dbname = "shopping_cart"; /* Database name */
+/* $dbname = "online_shop";  Database name */
 
 
 $con = mysqli_connect($host, $user, $password,$dbname);
@@ -18,18 +18,21 @@ if (!$con) {
 }
 
 
-//$data = json_decode(file_get_contents("php://input"));
-//$request = $data->request;
-
 $request = isset($_REQUEST['request'])?$_REQUEST['request']:"";
 
 // Register API
 if($request == "register")
 {
-	$first_name = isset($_REQUEST['first_name'])?$_REQUEST['first_name']:"";
-	$last_name = isset($_REQUEST['last_name'])?$_REQUEST['last_name']:"";
-	$email = isset($_REQUEST['email'])?$_REQUEST['email']:"";
-	$password =isset($_REQUEST['password'])?md5($_REQUEST['password']):"";
+	$data = json_decode(file_get_contents("php://input"));
+	$first_name = $data->password;
+	//echo "f name::".$first_name;
+	//echo "<pre>";
+	//print_r($_REQUEST);
+	//exit;
+	$first_name = isset($data->fname)?$data->fname:"";
+	$last_name = isset($data->lname)?$data->lname:"";
+	$email = isset($data->email)?$data->email:"";
+	$password =isset($data->password)?md5($data->password):"";
 	
 	if( !empty($first_name) &&  !empty($last_name) || !empty($email) || !empty($password)){
 		  $check_userData = mysqli_query($con,"select email as user_name from login_user where email ='".$email."'");
@@ -49,17 +52,21 @@ if($request == "register")
 // Login API
 if($request == "login")
 {
-	$email = isset($_REQUEST['email'])?$_REQUEST['email']:"";
-	$password =isset($_REQUEST['password'])?md5($_REQUEST['password']):"";
+	$data = json_decode(file_get_contents("php://input"));
+	
+	$email = isset($data->email)?$data->email:"";
+	$password =isset($data->password)?md5($data->password):"";
 	
 	if( !empty($email) &&  !empty($password) ){
-		  $userData = mysqli_query($con,"select first_name,last_name,email as user_name from login_user where email ='".$email."' AND password='".$password."'");
+		  //$userData = mysqli_query($con,"select first_name,last_name,email as user_name from login_user where email ='".$email."' AND password='".$password."'");
+		  $userData = mysqli_query($con,"select user_id from login_user where email ='".$email."' AND password='".$password."'");
 		  $login_user_data = array();
 		  if(mysqli_num_rows($userData)> 0){
 			  while($row = mysqli_fetch_assoc($userData)){
-				$login_user_data[] = $row;
+				  $loggedInuserId = $row['user_id'];
+				//$login_user_data[] = $row;
 			  }
-			  $response=array("status"=>"success","login_user_data"=>$login_user_data);
+			  $response=array("status"=>"success","login_user_id"=>$loggedInuserId);
 		  }else{
 			  $response=array("status"=>"fail","message"=>"Incorrect username and password , please try another time");
 		  }
@@ -90,11 +97,10 @@ if($request == 2)
 {
 	$product_id = isset($_REQUEST['product_id'])?$_REQUEST['product_id']:"";
 	$name = isset($_REQUEST['name'])?$_REQUEST['name']:"";
-	$rating = isset($_REQUEST['rating'])?$_REQUEST['rating']:"";
 	$comment =isset($_REQUEST['comment'])?$_REQUEST['comment']:"";
 	
 	if( !empty($product_id) &&  !empty($name) || !empty($rating) || !empty($comment)){
-		  $insert_review=mysqli_query($con,"INSERT INTO comments (product_id,name,rating,comment) VALUES('".$product_id."','".$name."','".$rating."','".$comment."')");
+		  $insert_review=mysqli_query($con,"INSERT INTO comments (product_id,name,comment) VALUES('".$product_id."','".$name."','".$comment."')");
 		  $response=array("status"=>"success","insert_review_id"=>mysqli_insert_id($con));
 	}else{
 		$response=array("status"=>"fail","message"=>"Please pass all field for add review");
@@ -123,29 +129,33 @@ if($request == 3)
 // Create Order API
 if($request == 4)
 {
-	$user_id = isset($_REQUEST['user_id'])?$_REQUEST['user_id']:"";
-	$name = isset($_REQUEST['name'])?$_REQUEST['name']:"";
-	$email_id = isset($_REQUEST['email_id'])?$_REQUEST['email_id']:"";
-	$mobile_no =isset($_REQUEST['mobile_no'])?$_REQUEST['mobile_no']:"";
-	$address =isset($_REQUEST['address'])?$_REQUEST['address']:"";
-	$product_id =isset($_REQUEST['product_id'])?$_REQUEST['product_id']:"";
-	$qty =isset($_REQUEST['qty'])?$_REQUEST['qty']:"";
-	$price =isset($_REQUEST['price'])?$_REQUEST['price']:"";
-	$order_total =isset($_REQUEST['order_total'])?$_REQUEST['order_total']:"";
+	$data = json_decode(file_get_contents("php://input"));
 	
+	$user_id = isset($data->userId)?$data->userId:"";
+	$name = isset($data->name)?$data->name:"";
+	$mobile_no =isset($data->mobile)?$data->mobile:"";
+	$address =isset($data->address)?$data->address:"";
+	$order_total =100;
 	
-	if( !empty($user_id) && !empty($name) && !empty($email_id) && !empty($mobile_no) && !empty($address) && !empty($mobile_no) && !empty($product_id) && !empty($qty) && !empty($price) && !empty($order_total))
+	/*echo "user id::".$user_id;
+	echo "name::".$name;
+	echo "mobile no::".$mobile_no;
+	echo "address::".$address;
+	echo "name::".$order_total;
+	*/
+	if( !empty($user_id) && !empty($name) && !empty($mobile_no) )
 	{	
-
-		 $insert_order=mysqli_query($con,"INSERT INTO product_order (user_id,name,mobile_no,address,order_total,order_date) VALUES('".$user_id."','".$name."','".$mobile_no."','".$address."','".$order_total."','".date('Y-m-d')."')");
+		//echo "inside if";
+		//exit;
+		 $insert_order=mysqli_query($con,"INSERT INTO product_order (user_id,name,mobile_no,address,order_total) VALUES('".$user_id."','".$name."','".$mobile_no."','".$address."','".$order_total."')");
 		 $order_id =mysqli_insert_id($con);
-		 for($i=0; $i<count($product_id); $i++)
+		 /*for($i=0; $i<count($product_id); $i++)
 		 {
 			 $product_id_data =$product_id[$i];
 			 $qty_data =$qty[$i];
 			 $price_data =$price[$i];
 			 $insert_review=mysqli_query($con,"INSERT INTO  product_order_qty (order_id,product_id,qty,price) VALUES('".$order_id."','".$product_id_data."','".$qty_data."','". $price_data."')");
-		 }
+		 }*/
 		 $response=array("status"=>"success","insert_order_id"=>$order_id);
 	}else{
 		$response=array("status"=>"fail","message"=>"Please pass all field for add order");
